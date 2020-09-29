05ee624b2414cb335b608f2773fbb47dbb5cbf5e
thekodecs <thekodecs@yahoo.com>
2020-09-29 16:28:32 +0300

    api get all questions in /questions

--- server/queries.js
+++ server/queries.js
@@ -6,3 +6,16 @@ const pool = new Pool({
   password: process.env.psql_pwd,
   port: 5432,
 });
+
+const getUsers = (request, response) => {
+    pool.query(`SELECT * FROM questions ORDER BY id ASC`, (error, results) => {
+      if (error) {
+        throw error
+      }
+      response.status(200).json(results.rows)
+    })
+  }
+
+  module.exports = {
+      getUsers
+  }
