1fe68351725c13542eea746e50c47b40541aabdf
thekodecs <thekodecs@yahoo.com>
2020-09-29 15:38:29 +0300

    swap

--- server/queries.js
+++ server/queries.js
@@ -1,19 +1,8 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const app = express();
-const port = 3000;
-
-app.use(bodyParser.json())
-app.use(
-    bodyParser.urlencoded({
-    extended: true,
-  })
-)
-
-app.get('/', (request, response) => {
-    response.json({ info: 'Node.js, Express, and Postgres API' })
-  })
-
-app.listen(port, () => {
-    console.log(`App running on port ${port}...`)
-  })
+const Pool = require('pg').Pool;
+const pool = new Pool({
+  user: 'me',
+  host: 'localhost',
+  database: 'api',
+  password: 'password',
+  port: 5432,
+});
