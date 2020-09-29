a550070854ba85f28af7e563cc50fc545f00848c
thekodecs <thekodecs@yahoo.com>
2020-09-29 21:31:04 +0300

    client node

---
+++ client/client.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+app = express();
+const port = 3001;
+app.use(cors());
+
+app.get('/', (request, response) => {
+    response.sendFile(path.join(__dirname+'/index.html'));
+});
+
+app.listen(port);
